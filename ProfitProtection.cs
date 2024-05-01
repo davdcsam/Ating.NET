d5f5723dae5a516027f1475336b1d505be64a0d8
davdcsam <edavidcamposl@gmail.com>
2024-04-30 22:08:56 -0600

    feat: Add profit protection functionality
    Introduce ProfitProtection class with BreakEven and TrailingStop subclasses to modify position stop loss and take profit levels. This allows for automatic profit protection based on percentage deviation and activation criteria. Addresses the need for dynamic risk management in trading strategies.

---
+++ ProfitProtection.cs
@@ -0,0 +1,72 @@
+﻿using cAlgo.API;
+
+namespace ProfitProtection
+{
+    public enum TypeProfitProtection
+    {
+        BreakEven,
+        TrailingStop
+    }
+
+    public class ProfitProtection
+    {
+
+        protected readonly Robot _cbot;
+        protected double PercentActivation;
+        protected double PercentDeviation;
+        protected string LabelIdentifier;
+        public ProfitProtection(Robot robot, double percentActivation, double percentDeviation, string labelIdentifier)
+        {
+            _cbot = robot;
+            PercentActivation = percentActivation;
+            PercentDeviation = percentDeviation;
+            LabelIdentifier = labelIdentifier;
+        }
+
+        protected bool IsValid(Position position) { return (position.Symbol.Name == _cbot.Symbol.Name && position.Label == LabelIdentifier); }
+    }
+    public class BreakEven : ProfitProtection
+    {
+        public BreakEven(Robot robot, double percentActivation, double percentDeviation, string labelIdentifier) : base(robot, percentActivation, percentDeviation, labelIdentifier) { }
+
+        public void Run()
+        {
+            Positions Positions = _cbot.Positions;
+
+            if (!Positions.Any()) return;
+
+            foreach (Position position in Positions)
+            {
+                if (!position.TakeProfit.HasValue || !IsValid(position)) continue;
+
+                double priceActivation = position.EntryPrice + ((position.TakeProfit.Value - position.EntryPrice) * PercentActivation / 100);
+                double newStopLoss = position.EntryPrice - (position.EntryPrice - position.TakeProfit.Value) * PercentDeviation / 100;
+
+                if (position.TradeType == TradeType.Buy && position.CurrentPrice >= priceActivation) { _cbot.ModifyPosition(position, newStopLoss, position.TakeProfit); }
+
+                if (position.TradeType == TradeType.Sell && position.CurrentPrice <= priceActivation) { _cbot.ModifyPosition(position, newStopLoss, position.TakeProfit); }
+            }
+        }
+    }
+    public class TrailingStop : ProfitProtection
+    {
+        public TrailingStop(Robot robot, double percentActivation, double percentDeviation, string labelIdentifier) : base(robot, percentActivation, percentDeviation, labelIdentifier) { }
+
+        public void Run()
+        {
+            if (!_cbot.Positions.Any()) return;
+
+            foreach (Position position in _cbot.Positions)
+            {
+                if (!position.TakeProfit.HasValue || !IsValid(position)) continue;
+
+                double priceActivation = position.EntryPrice + ((position.TakeProfit.Value - position.EntryPrice) * PercentActivation / 100);
+                double newStopLoss = position.CurrentPrice + (position.EntryPrice - position.TakeProfit.Value) * PercentDeviation / 100;
+
+                if (position.TradeType == TradeType.Buy && position.CurrentPrice >= priceActivation) { _cbot.ModifyPosition(position, newStopLoss, position.TakeProfit); }
+
+                if (position.TradeType == TradeType.Sell && position.CurrentPrice <= priceActivation) { _cbot.ModifyPosition(position, newStopLoss, position.TakeProfit); }
+            }
+        }
+    }
+}
