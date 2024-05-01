28ec8d91d9d16f1c803396e189a0e7f064d9fade
davdcsam <edavidcamposl@gmail.com>
2024-04-30 22:12:34 -0600

    Get iHighest and iLower (from MQL5 idea) to CTrader Automate
    Add Limits class for calculating highest and lowest prices for specified periods and dates. Adapted from the MQL5 idea to CTrader Automate.

---
+++ Limits.cs
@@ -0,0 +1,46 @@
+﻿using cAlgo.API;
+
+
+namespace Limits
+{
+    public class Limits
+    {
+        private readonly Robot _cbot;
+        public double Highest { get; private set; }
+        public double Lowest { get; private set; }
+        public TimeFrame TimeFrame { get; private set; }
+
+        public Limits(Robot robot, TimeFrame timeFrame)
+        {
+            _cbot = robot;
+            TimeFrame = timeFrame ?? TimeFrame.Minute;
+            Highest = double.MinValue;
+            Lowest = double.MinValue;
+        }
+
+        public void Get(int periods)
+        {
+            if (periods < 1) { _cbot.Print($"Periods arg '{periods}' under 1"); return; }
+
+            var bars = _cbot.MarketData.GetBars(TimeFrame);
+
+            Highest = bars.HighPrices.Maximum(periods);
+
+            Lowest = bars.LowPrices.Minimum(periods);
+        }
+
+        public void Get(DateTime start, DateTime end)
+        {
+            if (start >= end) { _cbot.Print($"Time inverted, start {start} over end {end}"); return; }
+
+            var bars = _cbot.MarketData.GetBars(TimeFrame);
+
+            var filteredBars = bars.Where(bar => bar.OpenTime >= start && bar.OpenTime <= end);
+
+            Highest = filteredBars.Max(bar => bar.High);
+
+            Lowest = filteredBars.Min(bar => bar.Low);
+        }
+    }
+
+}
